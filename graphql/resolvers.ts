c185ebf9dec3f9476eaf22b599ebcdc366021cbc
golamrabbiazad <golamrabbiazad@gmail.com>
2020-12-12 01:23:14 +0600

    added typed to resolvers

--- graphql/resolvers.ts
+++ graphql/resolvers.ts
@@ -2,9 +2,10 @@ import { getMovies, getMovie, getSuggestion } from "./db";
 
 const resolvers = {
   Query: {
-    movies: (_: any, { rating, limit }: any) => getMovies(limit, rating),
-    movie: (_: any, { id }: any) => getMovie(id),
-    suggestion: (_: any, { id }: any) => getSuggestion(id),
+    movies: (_: any, { rating, limit }: { rating: number; limit: number }) =>
+      getMovies(limit, rating),
+    movie: (_: any, { id }: { id: number }) => getMovie(id),
+    suggestion: (_: any, { id }: { id: number }) => getSuggestion(id),
   },
 };
 
